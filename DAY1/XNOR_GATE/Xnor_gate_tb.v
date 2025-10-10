59c821c99f7bcfca7868466f0b2539efa7e133ca
Selva Narendran S <140179772+SelvaNarendranS@users.noreply.github.com>
2025-10-11 00:58:50 +0530

    Xnor_gate_testbench

---
+++ DAY1/XNOR_GATE/Xnor_gate_tb.v
@@ -0,0 +1,25 @@
+// Xnor gate testbench
+
+`timescale 1ns/1ps
+
+module xnor_gate_tb();
+  wire c;
+  reg a, b;
+
+  // instantiation
+  xnor_gate xnor1 ( .a(a), .b(b), .c(c));
+
+  initial begin
+    $monitor("Time = %0t | a = %b | b = %b | c = %b ",$time, a, b, c);
+    $dumpfile("xnor_gate_dump.vcd");
+    $dumpvars;
+
+    // combinations
+    a = 0; b = 0; #5
+    a = 0; b = 1; #5
+    a = 1; b = 0; #5
+    a = 1; b = 1; #5
+    $finish;
+  end
+
+endmodule
