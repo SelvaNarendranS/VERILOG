44c42ee7ee3de6f9f6267b9baf7f8ffe2e2ea247
Selva Narendran S <140179772+SelvaNarendranS@users.noreply.github.com>
2025-10-11 12:42:38 +0530

    Gate level Half_subractor

--- DAY2/HALF_SUBRACTOR/Half_subractor.v
+++ DAY2/HALF_SUBRACTOR/Half_subractor.v
@@ -1,7 +1,8 @@
 // // Gate level implementation
 
 module half_adder(input a, b, output difference, borrow);
-
+  wire not_a;
+  
   /* assign difference = a ^ b;
      assign borrow = ~a & b; */
   xor(difference, a, b);
