2f17976412f2d594ca0f18863ad0a4c8a9adbf2b
Selva Narendran S <140179772+SelvaNarendranS@users.noreply.github.com>
2025-10-15 00:39:13 +0530

    Even_parity_generator_dataflow

--- DAY3/PARITY_GENERATOR/EVEN_PARITY_GENERATOR/Even_parity_generator_dataflow.v
+++ DAY3/PARITY_GENERATOR/EVEN_PARITY_GENERATOR/Even_parity_generator_dataflow.v
@@ -2,9 +2,11 @@
 
 module even_parity_generator (
 input [3:0]a,
-output b);
+output b,
+output [4:0]generated);
 
 	// using reduction operator
 	assign b = ^a;
+	assign generated = {b,a};
 
 endmodule
