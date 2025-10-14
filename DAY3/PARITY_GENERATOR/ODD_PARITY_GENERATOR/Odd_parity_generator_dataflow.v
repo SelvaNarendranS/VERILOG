906720948dfe57497049c42f7c9bdd914293d1c7
Selva Narendran S <140179772+SelvaNarendranS@users.noreply.github.com>
2025-10-15 00:43:44 +0530

    Odd_parity_generator_dataflow

--- DAY3/PARITY_GENERATOR/ODD_PARITY_GENERATOR/Odd_parity_generator_dataflow.v
+++ DAY3/PARITY_GENERATOR/ODD_PARITY_GENERATOR/Odd_parity_generator_dataflow.v
@@ -2,9 +2,12 @@
 
 module odd_parity_generator (
 input [3:0]a,
-output b);
+output b,
+output [4:0]generated);
 
 	// using reduction operator
 	assign b = ~^a;
+	assign generated = {b,a};
+
 
 endmodule
