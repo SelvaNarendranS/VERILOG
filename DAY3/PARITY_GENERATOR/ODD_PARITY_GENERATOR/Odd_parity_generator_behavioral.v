baec018f2dafbbc3bb898531b137bc7e5ad1c860
Selva Narendran S <140179772+SelvaNarendranS@users.noreply.github.com>
2025-10-15 00:42:43 +0530

    Odd_parity_generator_behavioral

--- DAY3/PARITY_GENERATOR/ODD_PARITY_GENERATOR/Odd_parity_generator_behavioral.v
+++ DAY3/PARITY_GENERATOR/ODD_PARITY_GENERATOR/Odd_parity_generator_behavioral.v
@@ -2,12 +2,14 @@
 
 module odd_parity_generator (
 input [3:0]a,
-output reg b);
+output reg b,
+output reg [4:0]generated);
 
 	always @(*) begin
 
 	b = ~^a; // using reduction operator
-
+	generated = {b,a};
+		
 	end
 
 endmodule
