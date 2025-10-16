dfce0b897a9ada5a96d0f9f1690c544bbd50d889
Selva Narendran S <140179772+SelvaNarendranS@users.noreply.github.com>
2025-10-16 22:33:35 +0530

    gray code_to_binary

---
+++ DAY4/GRAYCODE_TO_BINARY/Gray_to_binary.v
@@ -0,0 +1,16 @@
+// Behavioral - 4 bit
+
+module gray_to_binary(
+input [3:0] a,
+output reg [3:0]b);
+
+	always @(*) begin
+	
+	b[3] = a[3]; 
+	b[2] = b[3] ^ a[2];
+	b[1] = b[2] ^ a[1];
+	b[0] = b[1] ^ a[0];
+
+	end
+
+endmodule
