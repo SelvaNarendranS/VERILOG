fa19986a81c9ebedfac3b3f40c2f87439fc0b44c
Selva Narendran S <140179772+SelvaNarendranS@users.noreply.github.com>
2025-10-29 23:18:35 +0530

    Johnson counter test bench

---
+++ DAY7/JOHNSON_COUNTER/Nbit_sy_johnson_counter_tb.v
@@ -0,0 +1,41 @@
+// N bit synchronous Johnson - counter test bench
+
+`timescale 1ns/1ps
+
+module sy_johnsoncnt_tb;
+parameter N = 4;
+wire [N-1:0]q;
+reg clk, rst, enable;
+
+	// Instantiation
+	sy_johnsoncnt #(.N(N)) uup(.clk(clk), .rst(rst), .q(q), .enable(enable));
+
+	initial clk = 0;
+	always #5 clk = ~clk;
+
+	always @(posedge clk) begin
+		$display("Time = %0t | clk = %b | rst = %b | enable = %b | q = %b ",$time, clk, rst, enable, q);
+		end
+
+	initial begin
+		$dumpfile("Nbit_sy_up_counter_dump.vcd");
+		$dumpvars;
+
+		// initial reset
+		rst = 1;
+		enable = 0; #5;
+
+		// Deassert reset
+		#10 rst = 0;
+
+		// test case
+		repeat(15) begin
+		clk = 1;
+		enable = 1; #10;
+	        end	
+
+		#40 $finish;
+	end
+endmodule
+
+
