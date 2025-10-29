c864de0e4f482dcfa904b1251826da82f71f3129
Selva Narendran S <140179772+SelvaNarendranS@users.noreply.github.com>
2025-10-29 23:18:09 +0530

    Johnson counter

---
+++ DAY7/JOHNSON_COUNTER/Nbit_sy_johnson_counter.v
@@ -0,0 +1,14 @@
+// N bit synchronous Johnson counter 
+
+module sy_johnsoncnt #(parameter N = 4)(
+input clk, rst, enable,
+output reg [N-1:0]q);
+
+	always @(posedge clk) begin    // For synchronous reset only applied at clockedge on Q
+		if(rst)
+			q <= {N{1'b0}};
+		else if (enable)
+			q <= {~q[0],q[N-1:1]};
+	end
+
+endmodule
