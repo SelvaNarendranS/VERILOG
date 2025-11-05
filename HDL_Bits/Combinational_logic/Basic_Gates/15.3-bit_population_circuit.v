390424f6ea34fc1de9a9d1a3596b3d944d9ca53c
Selva Narendran S <140179772+SelvaNarendranS@users.noreply.github.com>
2025-11-05 23:09:14 +0530

    popcount3

---
+++ HDL_Bits/Combinational_logic/Basic_Gates/15.3-bit_population_circuit.v
@@ -0,0 +1,15 @@
+module top_module( 
+    input [2:0] in,
+    output [1:0] out );
+    
+    always @(*) begin
+        out = 1'b0;
+        for (integer i = 0; i < 3; i++) begin
+            if (in[i] == 1)
+                out = out + 1'b1;
+            else;
+        end
+    end
+
+endmodule
+
