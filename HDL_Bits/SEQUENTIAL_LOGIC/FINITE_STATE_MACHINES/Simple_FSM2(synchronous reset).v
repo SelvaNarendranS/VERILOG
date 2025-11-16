616c58ccdb074eda96f859738f8f51f97b112c1d
Selva Narendran S <140179772+SelvaNarendranS@users.noreply.github.com>
2025-11-16 23:29:13 +0530

    Fsm2s

---
+++ HDL_Bits/SEQUENTIAL_LOGIC/FINITE_STATE_MACHINES/Simple_FSM2(synchronous reset).v
@@ -0,0 +1,29 @@
+module top_module(
+    input clk,
+    input reset,    // Synchronous reset to OFF
+    input j,
+    input k,
+    output out); //  
+
+    parameter OFF=0, ON=1; 
+    reg state, next_state;
+
+    always @(*) begin
+        next_state <= 1'bx;// State transition logic
+        case(state)
+            OFF : next_state <= (j == 1) ? ON : OFF;
+            ON  : next_state <= (k == 1) ? OFF : ON;
+        endcase
+    end
+
+    always @(posedge clk) begin      
+        if(reset) 					// State flip-flops with synchronous reset
+            state <= OFF;
+        else
+            state <= next_state;
+    end
+
+    // Output logic
+    assign out = (state == ON);
+
+endmodule
