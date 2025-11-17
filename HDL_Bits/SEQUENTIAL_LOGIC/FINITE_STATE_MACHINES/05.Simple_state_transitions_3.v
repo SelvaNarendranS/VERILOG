b76f33425ebafe629685192069ca3aff00af304e
Selva Narendran S <140179772+SelvaNarendranS@users.noreply.github.com>
2025-11-17 22:56:20 +0530

    Fsm3comb

---
+++ HDL_Bits/SEQUENTIAL_LOGIC/FINITE_STATE_MACHINES/05.Simple_state_transitions_3.v
@@ -0,0 +1,22 @@
+module top_module(
+    input in,
+    input [1:0] state,
+    output [1:0] next_state,
+    output out); 
+
+    parameter A = 0, B = 1, C = 2, D = 3;
+    reg [1:0]state_in;
+    // State transition logic: next_state = f(state, in)
+    always @(*) begin
+        case(state)
+            A : next_state <= (in == 0) ? A : B;
+            B : next_state <= (in == 0) ? C : B;
+            C : next_state <= (in == 0) ? A : D;
+            D : next_state <= (in == 0) ? C : B;  
+        endcase
+    end
+
+    // Output logic:  out = f(state) for a Moore state machine
+    assign out = (state == D); 
+
+endmodule
