20ae4e46a3b1f8d0a4c9bfd5cc2ac819507cdef3
Selva Narendran S <140179772+SelvaNarendranS@users.noreply.github.com>
2025-11-19 22:39:14 +0530

    MUX_by_casex

---
+++ VERILOG_ASSIGNMENT/ASSIGNMENT_3/F.Case_Casex_Casez_Statements/46.MUX_by_casex.v
@@ -0,0 +1,34 @@
+// Modify the design to use casex to ignore undefined (x) bits.
+
+module mux4_casex;
+reg [3:0]a;
+reg [1:0]select_line;
+reg b;
+
+	always@(*) begin
+	
+		casex(select_line)
+	
+		2'b0x : b = a[3];
+		2'b10 : b = a[1];
+		2'b11 : b = a[0];
+		default : b = 2'b00;
+	
+		endcase
+	end
+
+	initial begin
+
+	$monitor("Time = %0t, a = %b select line = %b, b = %b", $time, a, select_line, b);
+
+	// 16 - Conditions(only testing 4)
+	a = 4'b1010;
+	select_line = 2'b00;  #5 
+	select_line = 2'b01;  #5 
+	select_line = 2'b10;  #5 
+	select_line = 2'b11;  #5
+
+	$finish;
+	end
+
+endmodule
