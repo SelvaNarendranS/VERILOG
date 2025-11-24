34261b7003e3fafdf7d03aa4135d1a514ed2417f
Selva Narendran S <140179772+SelvaNarendranS@users.noreply.github.com>
2025-11-24 23:22:39 +0530

    concatenate

---
+++ VERILOG_ASSIGNMENT/ASSIGNMENT_2/13.concatenate.v
@@ -0,0 +1,20 @@
+// 13. Find the value of b in the below snippet.
+/*
+reg [3:0]a;
+reg [6:0]b;
+	initial begin
+		a=4'd10;
+		b={a,1};
+	end	*/
+
+// ANS:
+
+module concatenate;
+reg [3:0]a;
+reg [6:0]b;
+	initial begin
+		a=4'd10;
+		b={a,1'b1};
+		$display("b({a,1}) = %b", b);
+	end
+endmodule
