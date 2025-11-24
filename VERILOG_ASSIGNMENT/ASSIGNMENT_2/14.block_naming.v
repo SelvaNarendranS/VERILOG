984b7514a98b8c93397aded78f18b8d2d4bc1880
Selva Narendran S <140179772+SelvaNarendranS@users.noreply.github.com>
2025-11-24 23:23:04 +0530

    block_naming

---
+++ VERILOG_ASSIGNMENT/ASSIGNMENT_2/14.block_naming.v
@@ -0,0 +1,22 @@
+// 14. Which block executes in the below snippet and justify the reason:
+/*
+reg [3:0]a= 4'b110x;
+	initial begin
+		if (a==4'b1100) begin: B1
+		end
+		else begin: B2 end
+	end	*/
+
+// ANS:
+
+module block_naming;
+reg [3:0]a= 4'b110x;
+	initial begin
+		if (a==4'b1100) begin: B1
+			$display("(a==4'b1100) so Block B1");
+		end
+		else begin: B2 
+			$display("(a!=4'b1100) so Block B2");
+		end
+	end
+endmodule
