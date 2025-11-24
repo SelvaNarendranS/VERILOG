e7821c4162670576f60b51e91a847367ae1c3664
Selva Narendran S <140179772+SelvaNarendranS@users.noreply.github.com>
2025-11-24 23:11:33 +0530

    bitwise_or

---
+++ VERILOG_ASSIGNMENT/ASSIGNMENT_2/10.bitwise_or.v
@@ -0,0 +1,22 @@
+// 10. What is the value of x in the below snippet?
+/*
+reg [2:0] a, b;
+reg [2:0]x;
+	initial begin
+		a=3'd5;
+		b=3’b111;
+		x= a | b;
+	end 	*/
+
+// ANS:
+
+module bitwise_or;
+reg [2:0] a, b;
+reg [2:0]x;
+	initial begin
+		a=3'd5;
+		b=3'b111;
+		x= a | b;
+		$display("x(a | b) = %b", x);
+	end
+endmodule
