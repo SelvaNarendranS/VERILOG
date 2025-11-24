8d531fd483600a8f7fc7b53ca6e220c33518effc
Selva Narendran S <140179772+SelvaNarendranS@users.noreply.github.com>
2025-11-24 23:11:08 +0530

    Logical_operator_and

---
+++ VERILOG_ASSIGNMENT/ASSIGNMENT_2/09.Logical_operator_and.v
@@ -0,0 +1,22 @@
+// 9. What is the value of y in the below snippet?
+/*
+reg [2:0] a, b;
+reg y;
+	initial begin
+		a= 3'd5;
+		b= 3'b111
+		y = a&&b;
+	end	*/
+
+// ANS:
+
+module value_y;
+reg [2:0] a, b;
+reg y;
+	initial begin
+		a= 3'd5;
+		b= 3'b111;
+		y = a && b;
+		$display("y(a && b) = %b", y);
+	end
+endmodule
