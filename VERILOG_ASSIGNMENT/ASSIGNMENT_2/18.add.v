a934de6fbd28da1449700c11c3f0075e611ebc36
Selva Narendran S <140179772+SelvaNarendranS@users.noreply.github.com>
2025-11-25 23:42:38 +0530

    add

---
+++ VERILOG_ASSIGNMENT/ASSIGNMENT_2/18.add.v
@@ -0,0 +1,17 @@
+// 18. What is the value of b in the below expression and justify the reason.
+/*
+reg [3:0]a= 4'b0100;
+reg [3:0]b;
+	initial begin
+		b = a+1'bx;
+	end		*/
+
+// ANS:
+module add;
+reg [3:0]a= 4'b0100;
+reg [3:0]b;
+	initial begin
+		b = a+1'bx;  // b = xxxx
+		$display("b = %b", b);
+	end
+endmodule
