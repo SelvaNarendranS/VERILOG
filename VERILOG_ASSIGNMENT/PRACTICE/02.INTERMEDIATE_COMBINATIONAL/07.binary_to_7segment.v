af990fc535987cab430104fc10a072b762c6c94a
Selva Narendran S <140179772+SelvaNarendranS@users.noreply.github.com>
2026-05-28 22:02:04 +0530

    binary to 7-segment
    Implemented a binary to 7-segment display converter and its testbench.

---
+++ VERILOG_ASSIGNMENT/PRACTICE/02.INTERMEDIATE_COMBINATIONAL/07.binary_to_7segment.v
@@ -0,0 +1,65 @@
+// binary to 7 segment
+
+// dut
+// Behavioral model
+
+module binary_to_7segment(
+input [3:0]a,
+output reg [7:0]b);
+
+	always @(*) begin
+		b = 8'b00000000;
+
+		case(a)      //abcdefgdt
+          4'b0000: b = 8'b11111100;  // 0  lsb equal to dot rest bits is for 7 segment
+          4'b0001: b = 8'b01100000;  // 1
+          4'b0010: b = 8'b11011010;  // 2
+          4'b0011: b = 8'b11110010;  // 3
+          4'b0100: b = 8'b01100110;  // 4
+          4'b0101: b = 8'b10110110;  // 5
+          4'b0110: b = 8'b10111110;  // 6
+          4'b0111: b = 8'b11100000;  // 7
+          4'b1000: b = 8'b11111110;  // 8
+          4'b1001: b = 8'b11100110;  // 9
+          4'b1010: b = 8'b11101110;  // A
+          4'b1011: b = 8'b00111110;  // B
+          4'b1100: b = 8'b10011100;  // C
+          4'b1101: b = 8'b01111010;  // D
+          4'b1110: b = 8'b10011110;  // E
+          4'b1111: b = 8'b10001110;  // F
+		default: b = 8'b00000000;
+		endcase
+	end
+
+endmodule 
+	
+
+// testbench
+// binary_to_7segment Testbench
+
+`timescale 1ns/1ps
+
+module binary_to_7segment_tb;
+wire [7:0]b;
+reg [3:0]a;
+integer i;
+
+	// instantation 
+	binary_to_7segment ct7s(.a(a), .b(b));
+
+	initial begin
+	
+	$monitor("Time = %0t | a = %b |  b = %b (%h) ", $time, a, b, b);
+	$dumpfile("binary_to_7segment_dump.vcd");
+	$dumpvars;
+
+		// conditions
+		for(i = 0; i < 2**4; i++) begin
+		a = i;
+		#5;
+		end
+
+	$finish;
+	end
+
+endmodule
