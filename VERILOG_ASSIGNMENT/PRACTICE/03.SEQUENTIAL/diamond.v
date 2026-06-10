7a0f50baf0289c7b573f82e596734e105e59faf6
Selva Narendran S <140179772+SelvaNarendranS@users.noreply.github.com>
2026-06-10 23:49:51 +0530

    diamond
    Implement diamond pattern printing in Verilog.

---
+++ VERILOG_ASSIGNMENT/PRACTICE/03.SEQUENTIAL/diamond.v
@@ -0,0 +1,36 @@
+module diamond;
+  reg [7:0] value;
+  reg [3:0] range;
+  integer i, j, k;
+  
+  initial begin
+    //  upper half
+    for(i = range; i >= 1; i--) begin
+      for(j = 1; j < i; j++) begin
+        $write(" ");
+      end
+      for(k = i; k <= range; k++) begin
+        $write("%0s ", value);
+      end
+      $display();
+    end
+    
+    // lower half
+    for(i = 1; i < range-1; i++) begin
+      for(j = 1; j < i; j++) begin
+        $write(" ");
+      end
+      for(k = i; k < range; k++) begin
+        $write("%0s ", value);
+      end
+      $display();
+    end
+  end
+  
+  initial begin
+    value = "*";
+    range = 5;
+  end
+  
+endmodule
+        
